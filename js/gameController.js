4155b85246c1e6f22b2ede762fef3deda7190c1f
Travis <travis.m.bruce@gmail.com>
2022-06-07 20:39:19 -0700

    Work on winning line indicator

--- js/gameController.js
+++ js/gameController.js
@@ -30,31 +30,56 @@ const GameController = (() => {
     events.emit("resetBoard");
   };
 
-  const calcLine = (point1, point2) => {
-    let gameContainer = document.querySelector(".game-container");
-    // gameContainer.getBoundingClientRect()
-    let length = Math.round(
-      Math.sqrt(
-        Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2)
-      )
-    );
-    let angle = Math.round(
-      Math.atan2(point2.y - point1.y, point2.x - point1.x) * (180 / Math.PI)
-    );
+  const calcLine = (point1, point2, vec) => {
+    let length =
+      Math.round(
+        Math.sqrt(
+          Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2)
+        ) / 5
+      ) * 5;
+    let angle = 0;
+    if (vec[1] == 1 || vec[1] == 7) angle = -90;
+    if (vec[1] == 3 || vec[1] == 5) angle = 0;
+    if (vec[1] == 4 && vec[0] == 0) angle = 45;
+    if (vec[1] == 4 && vec[0] == 2) angle = -45;
+    if (vec[1] == 4 && vec[0] == 3) angle = -90;
     return { length, angle };
   };
 
   const declareWinner = (vec) => {
+    const LINE_WIDTH = 10;
     let strikeThrough = document.querySelector("#strike-through");
+    let gameContainer = document.querySelector(".game-container");
     let winningCells = [];
     vec.forEach((point) => {
       winningCells.push(document.getElementById(`cell-${parseInt(point) + 1}`));
     });
-    console.log(winningCells[0].getBoundingClientRect());
     let { length, angle } = calcLine(
       winningCells[0].getBoundingClientRect(),
-      winningCells[2].getBoundingClientRect()
+      winningCells[2].getBoundingClientRect(),
+      vec
     );
+    let container = gameContainer.getBoundingClientRect();
+    let center = winningCells[1].getBoundingClientRect();
+    let cY = center.top - container.top + 0.5 * center.height;
+    let cX =
+      center.left - container.left + 0.5 * center.width - 0.5 * LINE_WIDTH;
+    let style = `top: ${cY}px; left: ${cX}px; transform: rotate(${
+      angle * -1
+    }deg) scaleY(${length * 1.5}); height: 1px; width: 10px; position: absolute;
+    transform-origin: center;
+    z-index: 1;
+    background-color: white;
+    display: block;`;
+    strikeThrough.style.cssText = style;
+    setTimeout(() => {
+      let modal = document.querySelector("#modal");
+      let div = document.createElement("div");
+      div.textContent = `${winningCells[0].textContent} wins!`;
+      div.classList.add("win-message");
+      modal.append(div);
+      modal.showModal();
+    }, 600);
   };
 
   const checkWinner = () => {
